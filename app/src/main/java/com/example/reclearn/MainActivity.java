42dc1d4d3c75468b03a81f02537088561ed2025d
chanchaljai <chanchal123jaiswal@gmail.com>
2024-03-22 21:25:52 +0530

    recyclerView Ok

--- app/src/main/java/com/example/reclearn/MainActivity.java
+++ app/src/main/java/com/example/reclearn/MainActivity.java
@@ -6,13 +6,40 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    recyclerContactAdapter adapter;
+    ArrayList<contactModel> arrContacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        arrContacts.add(new contactModel(R.drawable.a,"A","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.b,"B","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.c,"C","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.a,"A","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.b,"B","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.c,"C","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.a,"A","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.b,"B","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.c,"C","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.a,"A","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.b,"B","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.c,"C","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.a,"A","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.b,"B","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.c,"C","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.a,"A","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.b,"B","9999999999"));
+        arrContacts.add(new contactModel(R.drawable.c,"C","9999999999"));
+
+        recyclerContactAdapter adapter = new recyclerContactAdapter(this,arrContacts);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/reclearn/recyclerContactAdapter.java
+++ app/src/main/java/com/example/reclearn/recyclerContactAdapter.java
@@ -1,31 +1,57 @@
 package com.example.reclearn;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class recyclerContactAdapter extends RecyclerView.Adapter<recyclerContactAdapter.ViewHolder> {
+    Context context;
+    ArrayList<contactModel> arrContacts;
+
+    public recyclerContactAdapter(Context context, ArrayList<contactModel> arrContacts) {
+        this.context = context;
+        this.arrContacts = arrContacts;
+    }
+
+
+
     @NonNull
     @Override
     public recyclerContactAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.contact,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull recyclerContactAdapter.ViewHolder holder, int position) {
+        holder.imgContact.setImageResource(arrContacts.get(position).img);
+        holder.txtName.setText(arrContacts.get(position).name);
+        holder.txtNumber.setText(arrContacts.get(position).number);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrContacts.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtName,txtNumber;
+        ImageView imgContact;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            txtName = itemView.findViewById(R.id.txtName);
+            txtNumber = itemView.findViewById(R.id.txtNumber);
+            imgContact = itemView.findViewById(R.id.imgContact);
+
         }
     }
 }
